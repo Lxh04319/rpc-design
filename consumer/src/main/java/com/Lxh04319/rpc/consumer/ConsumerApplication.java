0e2a1adff83a38365b903d3240c42ac345cede7e
Lxh04319 <lxh3162335158@qq.com>
2024-06-19 22:14:52 +0800

    update

--- consumer/src/main/java/com/Lxh04319/rpc/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/Lxh04319/rpc/consumer/ConsumerApplication.java
@@ -1,4 +1,4 @@
-package com.Lxh04319.rpc.consumer.controller;
+package com.Lxh04319.rpc.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
