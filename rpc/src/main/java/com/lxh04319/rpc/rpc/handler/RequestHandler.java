86665ca42a02da5d02638a922b568d55d417cff7
Lxh04319 <lxh3162335158@qq.com>
2024-06-28 19:48:43 +0800

    update

---
+++ rpc/src/main/java/com/lxh04319/rpc/rpc/handler/RequestHandler.java
@@ -0,0 +1,38 @@
+package com.lxh04319.rpc.rpc.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * 进行过程调用的处理器
+ */
+public class RequestHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final ServiceProvider serviceProvider;
+
+    static {
+        serviceProvider = new ServiceProviderImpl();
+    }
+
+    public Object handle(RpcRequest rpcRequest) {
+        Object service = serviceProvider.getServiceProvider(rpcRequest.getInterfaceName());
+        return invokeTargetMethod(rpcRequest, service);
+    }
+
+    private Object invokeTargetMethod(RpcRequest rpcRequest, Object service) {
+        Object result;
+        try {
+            Method method = service.getClass().getMethod(rpcRequest.getMethodName(), rpcRequest.getParamTypes());
+            result = method.invoke(service, rpcRequest.getParameters());
+            logger.info("服务:{} 成功调用方法:{}", rpcRequest.getInterfaceName(), rpcRequest.getMethodName());
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            return RpcResponse.fail(ResponseCode.METHOD_NOT_FOUND, rpcRequest.getRequestId());
+        }
+        return result;
+    }
+
+}
